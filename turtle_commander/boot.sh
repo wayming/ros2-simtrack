75a4783f8091656fdd2f75b7488b9305ebc87237
way <wayming.z@gmail.com>
2025-07-06 04:56:01 +0000

    ignore

--- turtle_commander/boot.sh
+++ turtle_commander/boot.sh
@@ -9,5 +9,5 @@ docker run -d --rm \
   -e DISPLAY=$DISPLAY \
   -e QT_X11_NO_MITSHM=1 \
   -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
-  -v ..:/ros2_ws \
+  -v $(pwd)/../:/ros2_ws \
   turtle_commander:latest
