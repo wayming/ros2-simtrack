f2c2acd6c965dffd31fdf0c8e0271a311aba05bd
way <wayming.z@gmail.com>
2025-07-24 01:08:19 +1000

    update

--- waffle_tf2_nav_ws/run.sh
+++ waffle_tf2_nav_ws/run.sh
@@ -10,23 +10,23 @@ source /opt/ros/jazzy/setup.bash
 source install/setup.bash
 export TURTLEBOT3_MODEL=waffle
 
-ros2 launch turtle_autonomous_patrol patrol_launch.py
+ros2 launch waffle_tf2_navigation sim_tf2.launch.py
 
 ros2 launch turtlebot3_gazebo turtlebot3_world.launch.py &
-ros2 run turtle_autonomous_patrol monitor_node &
-ros2 run turtle_autonomous_patrol max_speed_service &
-ros2 run turtle_autonomous_patrol patrol_server &
+ros2 run waffle_tf2_navigation monitor_node &
+ros2 run waffle_tf2_navigation max_speed_service &
+ros2 run waffle_tf2_navigation patrol_server &
 ros2 topic echo /alert
 ros2 topic echo /cmd_vel geometry_msgs/msg/TwistStamped
 
-ros2 run turtle_autonomous_patrol patrol_client
+ros2 run waffle_tf2_navigation patrol_client
 ros2 service call /set_max_speed std_srvs/srv/SetBool "{data: true}"
 ros2 param set /max_speed_service max_speed 0.7
 ros2 topic pub /cmd_vel geometry_msgs/msg/TwistStamped "{header: {stamp: {sec: 0, nanosec: 0}, frame_id: ''}, twist: {linear: {x: 0.2, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 0.0}}}"拓展：
 
-ros2 launch turtle_autonomous_patrol patrol_launch.py
-GAZEBO_MODEL_PATH=:/ros2_ws/turtle_autonomous_patrol_ws/install/turtlebot3_description/share/turtlebot3_description ros2 launch turtle_autonomous_patrol patrol_launch.py
-export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/ros2_ws/turtle_autonomous_patrol_ws/install/turtlebot3_description/share/turtlebot3_description 
+ros2 launch waffle_tf2_navigation sim_tf2.launch.py
+GAZEBO_MODEL_PATH=:/ros2_ws/waffle_tf2_navigation_ws/install/turtlebot3_description/share/turtlebot3_description ros2 launch waffle_tf2_navigation patrol_launch.py
+export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/ros2_ws/waffle_tf2_navigation_ws/install/turtlebot3_description/share/turtlebot3_description 
 
- export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/ros2_ws/turtle_autonomous_patrol_ws/install/turtlebot3_description/share/
- export GZ_SIM_RESOURCE_PATH=$GZ_SIM_RESOURCE_PATH:/ros2_ws/turtle_autonomous_patrol_ws/install/turtlebot3_description/share/
+ export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/ros2_ws/waffle_tf2_navigation_ws/install/turtlebot3_description/share/
+ export GZ_SIM_RESOURCE_PATH=$GZ_SIM_RESOURCE_PATH:/ros2_ws/waffle_tf2_navigation_ws/install/turtlebot3_description/share/
--- waffle_tf2_nav_ws/src/waffle_tf2_navigation/launch/sim_tf2.launch.py
+++ waffle_tf2_nav_ws/src/waffle_tf2_navigation/launch/sim_tf2.launch.py
@@ -1,6 +1,7 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, TimerAction
 from launch.substitutions import LaunchConfiguration, Command, PathJoinSubstitution
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
@@ -14,7 +15,7 @@ def generate_launch_description():
     urdf_file = PathJoinSubstitution([
         FindPackageShare('turtlebot3_description'),
         'urdf',
-        'turtlebot3_waffle.urdf',
+        'turtlebot3_waffle.urdf'
     ])
 
     robot_description_content = Command([
@@ -29,36 +30,52 @@ def generate_launch_description():
         'tf2_perception.rviz'
     ])
 
+    gazebo_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([
+                FindPackageShare('turtlebot3_gazebo'),
+                'launch',
+                'turtlebot3_world.launch.py'
+            ])
+        ])
+    )
+
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='screen',
+        parameters=[{
+            'robot_description': robot_description_content,
+            'use_sim_time': True
+        }]
+    )
+
+    static_broadcaster_node = Node(
+        package='waffle_tf2_navigation',
+        executable='static_broadcaster',
+        output='screen'
+    )
+
+    listener_node = Node(
+        package='waffle_tf2_navigation',
+        executable='listener',
+        output='screen'
+    )
+
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        arguments=['-d', rviz_config_file],
+        output='screen'
+    )
+
     return LaunchDescription([
         namespace_arg,
-        
-        Node(
-            package='robot_state_publisher',
-            executable='robot_state_publisher',
-            name='robot_state_publisher',
-            output='screen',
-            parameters=[{
-                'robot_description': robot_description_content,
-                'use_sim_time': True
-            }]
-        ),
-
-        Node(
-            package='waffle_tf2_navigation',
-            executable='static_broadcaster',
-            output='screen'
-        ),
-
-        Node(
-            package='waffle_tf2_navigation',
-            executable='listener',
-            output='screen'
-        ),
-        Node(
-            package='rviz2',
-            executable='rviz2',
-            name='rviz2',
-            arguments=['-d', rviz_config_file],
-            output='screen',
-        )
+        gazebo_launch,
+        TimerAction(period=5.0, actions=[robot_state_publisher_node]),
+        TimerAction(period=8.0, actions=[static_broadcaster_node]),
+        TimerAction(period=11.0, actions=[listener_node]),
+        TimerAction(period=13.0, actions=[rviz_node]),
     ])
